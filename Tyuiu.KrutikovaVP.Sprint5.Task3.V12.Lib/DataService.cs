7600da3a36b96cf94d91a44c201f5dd895b07504
Tyuiu.KrutikovaVP@outlook.com <lera.krutikova.2005@mail.ru>
2023-11-18 00:03:27 +0500

    Task3

---
+++ Tyuiu.KrutikovaVP.Sprint5.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KrutikovaVP.Sprint5.Task3.V12.Lib
+{
+    public class DataService : ISprint5Task3V12
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            Double y = Math.Round(Math.Pow(x, 3) / (2 * Math.Pow(x + 5, 2)), 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KrutikovaVP.Sprint5.Task3.V12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KrutikovaVP.Sprint5.Task3.V12.Lib;
+
+namespace Tyuiu.KrutikovaVP.Sprint5.Task3.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Крутикова В. П. | АСОиУБ-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Обработка файлов                                                   *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #12                                                              *");
+            Console.WriteLine("* Выполнил: Крутикова Валерия Павловна | АСОиУБ-23-3                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить     *");
+            Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль.                *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                  *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            int x = 3;
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine($"Файл: {res}");
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
