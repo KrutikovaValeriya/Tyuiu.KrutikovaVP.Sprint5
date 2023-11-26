01c8c7f2f917f48287d6c0155fd0308280297c09
Tyuiu.KrutikovaVP@outlook.com <lera.krutikova.2005@mail.ru>
2023-11-26 17:02:27 +0500

    Task4

---
+++ Tyuiu.KrutikovaVP.Sprint5.Task4.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KrutikovaVP.Sprint5.Task4.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KrutikovaVP.Sprint5.Task4.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa21ca07-7560-4314-aa55-0c4850055a96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KrutikovaVP.Sprint5.Task4.V2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KrutikovaVP.Sprint5.Task4.V2.Lib;
+using System.IO;
+
+
+namespace Tyuiu.KrutikovaVP.Sprint5.Task4.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Крутикова В. П. | АСОиУБ-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Обработка файлов                                                   *");
+            Console.WriteLine("* Задание #4                                                               *");
+            Console.WriteLine("* Вариант #2                                                               *");
+            Console.WriteLine("* Выполнил: Крутикова Валерия Павловна | АСОиУБ-23-3                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask4V0.txt                       *");
+            Console.WriteLine("* в котором есть вещественное значение. Прочитать значение из файла        *");
+            Console.WriteLine("* подставить вместо Х в формуле. Вычислить значение по формуле             *");
+            Console.WriteLine("* и вернуть полученный результат на консоль.                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V2.txt";
+            Console.WriteLine("данные находятся в файле: " + path);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+           
+            Console.ReadKey();
+        }
+    }
+}
